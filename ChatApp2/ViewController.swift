bb68fa585fb2e8ca1e5d2b464de9fe6ed8cecedc
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-22 16:51:12 +0900

    Initial Commit

---
+++ ChatApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp2
+//
+//  Created by Tatsushi Fukunaga on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
