5c7991a98106fd8ec97ae5887fdd2801a2dbe6a7
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-02-22 17:07:17 +0900

    初期画面の設定

---
+++ ChatApp2/ChatListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatListViewController.swift
+//  ChatApp2
+//
+//  Created by Tatsushi Fukunaga on 2021/02/22.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
